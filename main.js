90e0af8d1ba20555132ae16b0802c75fd07338dd
welli363 <toncosta08@gmail.com>
2025-08-27 08:53:38 -0300

    melhoria do js
    mudei o script do arquivo main.js, vendo que antes os botões não estavam funcionando.

--- main.js
+++ main.js
@@ -1,78 +1,41 @@
+document.addEventListene('DOMContentLoaded'), ()=>{
+    // todas as telas 
+    const telas = document.querySelectorAll('.telaInicial > div');
+    let telaAtual = 0;
 
-  const tela0 = document.querySelector ('.tela0')
-  const tela1 = document.querySelector ('.tela1');
-  const tela2 = document.querySelector ('.tela2');
-  const tela3 = document.querySelector ('.tela3');
-  const tela4 = document.querySelector ('.tela4');
-  const tela5 = document.querySelector ('.tela5');
-  const tela6 = document.querySelector ('.tela6');
-  const tela7 = document.querySelector ('.tela7');
-  const tela8 = document.querySelector ('.tela8');
-  const tela9 = document.querySelector ('.tela9');
-  const tela10 = document.querySelector ('.tela10');
-  const telaFinal = document.querySelector ('.telaFinal');
-  const btnVoltarInicio = document.getElementById ('voltarInicio');
-  const respostaErrada = document.querySelectorAll ('.erro');
-
-  const todasTelas = [tela0, tela1, tela2, tela3, tela4, tela5, tela6, tela7, tela8, tela9,tela10, telaFinal,btnVoltarInicio];
-
-function mostrarUmaTela (telaParaMostrar){
-    todasTelas.forEach (tela => {
-        if (tela){
-            tela.style.display = 'none';
-        };
-    });
-    //mostrar a tela que eu quero
-    if(telaParaMostrar){
-        telaParaMostrar.style.display = 'inline-block'; //para mostrar a tela do if anterior
-    };
-};
-
-//evento do botão para começar a jogar
-document.getElementById ('btnComecar').onclick = function (){
-    mostrarUmaTela (tela1);
-};
-//evento para o acerto 
-document.getElementById ('respostaCerta1').onclick = function (){
-    mostrarUmaTela (tela2);
-};
-//para o acert2
-document.getElementById ('respostaCerta2').onclick =  function (){
-    mostrarUmaTela (tela3);
-};
-//acerto3
-document.getElementById ('respostaCerta3').onclick =  function (){
-    mostrarUmaTela (tela4);
-};
-//acerto4
-document.getElementById ('respostaCerta4').onclick =  function (){
-    mostrarUmaTela (tela5);
-};
-//acerto5
-document.getElementById ('respostaCerta5').onclick =  function (){
-    mostrarUmaTela (tela6);
-};
-document.getElementById ('respostaCerta6').onclick =  function (){
-    mostrarUmaTela (tela7);
-};
-document.getElementById ('respostaCerta7').onclick =  function (){
-    mostrarUmaTela (tela8);
-};
-document.getElementById ('respostaCerta8').onclick =  function (){
-    mostrarUmaTela (tela9);
-};
-document.getElementById ('respostaCerta9').onclick =  function (){
-    mostrarUmaTela (tela10);
-};
-document.getElementById ('respostaCerta10').onclick =  function (){
-    mostrarUmaTela (telaFinal);
-};
-document.getElementById('voltarInicio').onclick = function (){
-    mostrarUmaTela(tela0);
-} 
-//evento para as respostas erradas 
-respostaErrada.forEach (elementoErro => {
-    elementoErro.onclick = function(){
-        mostrarUmaTela(tela0);
+    //função para mostrar a tela atual
+    function mostrarTela(index){
+        telas.forEach((tela, i)=>{
+            tela.style.display = i === index ? 'block' : 'none';
+        });
     }
+};
+mostrarTela (telaAtual);
+//botão começar
+const btnComecar = document.getElementById('btnComecar').addEventListener('click', ()=>{
+    telaAtual++;
+    mostrarTela(telaAtual);
 });
+//adicionar evento em todas as respostas 
+telas.forEach((tela, index)=>{
+    const botoes = tela.querySelectorAll('button');
+    botoes.forEach(botao =>{
+        botao.addEventListene('click', ()=>{
+            //verificar botao certa
+            if(botao.id.startsWith('respostaCerta')){
+                alert('Correta resposta!')
+            }else if(botao.classList.contains('erro')){
+                alert('Resposta errada!')
+            }
+            //para a proxima tela 
+            if(index + 1 < telas.length - 1){
+                telaAtual = index+1;
+                mostrarTela(telaAtual);
+            }else{
+                //mostrar tela final
+                telaAtual = telas.length - 1;
+                mostrarTela(telaAtual);
+            }
+        })
+    })
+})
